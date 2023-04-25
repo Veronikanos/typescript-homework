58e5a5a110bd82aecf44c3e81c32465c9bb987e5
Veronica <vveronikanika@gmail.com>
2023-04-25 15:26:19 +0300

    feat: render posts to page using fetch

--- task1/main.ts
+++ task1/main.ts
@@ -1,1 +1,36 @@
-console.log('test');
+interface IPost {
+  body: string;
+  id: number;
+  title: string;
+  userId: number;
+}
+
+function renderPosts(posts: IPost[]): void {
+  const postsList: HTMLElement = document.querySelector('#posts')!;
+  posts.forEach((post: IPost) => {
+    const li: HTMLElement = document.createElement('li');
+    li.innerHTML = `
+      <h2>${post.title}</h2>
+			<span>User id: ${post.userId}</span>
+      <p>${post.body}</p>
+    `;
+    postsList?.appendChild(li);
+  });
+}
+
+async function getPosts(): Promise<IPost[]> {
+  const response: Response = await fetch(
+    'https://jsonplaceholder.typicode.com/posts'
+  );
+  const posts: IPost[] = await response.json();
+  return posts;
+}
+
+(async () => {
+  try {
+    const posts: IPost[] = await getPosts();
+    renderPosts(posts);
+  } catch (error) {
+    console.error(error);
+  }
+})();
